784d0ecb5c1ed36f9082a4fca05c8853d27e8fb2
Chike-Ode <chikeodenigbo@gmail.com>
2022-10-13 02:22:41 -0400

    Reached question 4

--- Code/functions.R
+++ Code/functions.R
@@ -7,10 +7,15 @@
 #install.packages("isoband", type="binary")
 #install.packages("farver", type="binary")
 #install.packages('ggplot2')
-install.packages('ROCR', type="binary")
-
+#install.packages('ROCR', type="binary")
+#install.packages('proxy', type="binary")
+#install.packages('e1071', type="binary")
+#install.packages('Rcpp', type="binary")
+#install.packages('rlang', type="binary")
+#install.packages('caret', type="binary")
 library("rstudioapi") 
 library("ggplot2")
+library(caret)
 library(ROCR)
 theme_set(theme_minimal())
 #install.packages("rmarkdown", dependencies=TRUE) 
@@ -76,6 +81,7 @@ perf_df_1_3 = mse_calculator(train = train_df, test = test_df, degree_range = 1:
 # Question 1.4
 perf_df_1_4 = mse_calculator(train = train_df, test = test_df, degree_range = 10:1, display_plot = TRUE, question_num = "1.4",include_test = TRUE)
 
+#Question 1.5
 # Lecture et preparation des données (voir document sur l'analyse de ces donnees)
 mydata=read.csv("Data/kc_house_data.csv")
 mydata$log_price=log(mydata$price)
@@ -111,13 +117,227 @@ mse_test1 = mean((test_df$y - predict.lm(model1, test_df)) ^ 2)
 
 
 
-
+###########################################
+#EXERCISE 2
+###########################################
 
 compas_df = read.csv("Data/Compas.csv", sep=";",header=T)
 compas_df$decil_score
 compas_df$two_year_recid
-compas_df$recidive_pred = ifelse(compas_df$decile_score>=6, 1, 0) 
-mean(compas_df$recidive_pred != compas_df$two_year_recid)
-pred <- prediction( compas_df$recidive_pred, compas_df$two_year_recid)
-tpr <- pred@tp[[1]]/max(pred@tp[[1]])
-fpr <- pred@fp[[1]]/max(pred@fp[[1]])
+compas_df$recidive_pred = factor(ifelse(compas_df$decile_score>=6, 1, 0)) 
+
+conf_matrix = confusionMatrix(data=factor(compas_df$recidive_pred), reference = factor(compas_df$two_year_recid))
+fp = conf_matrix$table[2,1]
+tp = conf_matrix$table[2,2]
+tn = conf_matrix$table[1,1]
+fn = conf_matrix$table[1,2]
+
+#Question 2.2
+# misclassification
+mis_rate = (fp + fn)/(fp + fn + tn +tp)
+# false positive rate
+fpr = fp / (fp + tn)
+# false discovery rates
+fdr = fp / (fp + tp)
+
+#Question 2.3
+compas_df$decile_score_scaled = compas_df$decile_score / 10
+compas_df$decile_score_scaled
+
+pred = prediction(compas_df$decile_score_scaled,compas_df$two_year_recid)
+perf <- performance(pred, "tpr", "fpr")
+cost_perf <- performance(pred, "cost")
+plot(perf,print.cutoffs.at=seq(0.1,by=0.1))
+plot(cost_perf)
+
+pred@cutoffs[[1]][which.min(cost_perf@y.values[[1]])]
+
+#Question 2.4
+afr_amer_df = subset(compas_df,race == 'African-American')
+conf_matrix_afr = confusionMatrix(data=factor(afr_amer_df$recidive_pred), reference = factor(afr_amer_df$two_year_recid))
+fp_afr = conf_matrix_afr$table[2,1]
+tp_afr = conf_matrix_afr$table[2,2]
+tn_afr = conf_matrix_afr$table[1,1]
+fn_afr = conf_matrix_afr$table[1,2]
+
+# misclassification
+mis_rate_afr = (fp_afr + fn_afr)/(fp_afr + fn_afr + tn_afr + tp_afr)
+# false positive rate
+fpr_afr = fp_afr / (fp_afr + tn_afr)
+# false discovery rates
+fdr_afr = fp_afr / (fp_afr + tp_afr)
+
+cauc_df = subset(compas_df,race == 'Caucasian')
+conf_matrix_cauc = confusionMatrix(data=factor(cauc_df$recidive_pred), reference = factor(cauc_df$two_year_recid))
+fp_cauc = conf_matrix_cauc$table[2,1]
+tp_cauc = conf_matrix_cauc$table[2,2]
+tn_cauc = conf_matrix_cauc$table[1,1]
+fn_cauc = conf_matrix_cauc$table[1,2]
+
+# misclassification
+mis_rate_cauc = (fp_cauc + fn_cauc)/(fp_cauc + fn_cauc + tn_cauc + tp_cauc)
+# false positive rate
+fpr_cauc = fp_cauc / (fp_cauc + tn_cauc)
+# false discovery rates
+fdr_cauc = fp_cauc / (fp_cauc + tp_cauc)
+
+# Question 2.5
+pred_afr_amer = prediction(afr_amer_df$decile_score_scaled,afr_amer_df$two_year_recid)
+cost_perf_afr_amer <- performance(pred_afr_amer, "cost")
+plot(cost_perf_afr_amer)
+
+pred_afr_amer@cutoffs[[1]][which.min(cost_perf_afr_amer@y.values[[1]])]
+
+pred_cauc = prediction(cauc_df$decile_score_scaled,cauc_df$two_year_recid)
+cost_perf_cauc <- performance(pred_cauc, "cost")
+plot(cost_perf_cauc)
+
+pred_afr_cauc@cutoffs[[1]][which.min(cost_perf_cauc@y.values[[1]])]
+
+###########################################
+#EXERCISE 3
+###########################################
+dbm_df = read.csv("Data/dbm_final.csv",header=T)
+dbm_df$region=as.factor(dbm_df$region)
+dbm_df$revenu=as.factor(dbm_df$revenu)
+model_dbm=glm(yachat~sexe + age + revenu + region + conjoint + anneeclient + semainedernier+montantdernier + montantunan + achatunan,family="binomial",data=dbm_df[dbm_df$train==1,] )
+summary(model_dbm)
+y_true = dbm_df$yachat
+y_pred = predict(model_dbm, dbm_df, type="response")
+
+min(y_pred)  
+max(y_pred)
+plot_roc = function(y_true,y_pred,increment = 0.02){
+  perf_df = data.frame(matrix(ncol = 3, nrow = 0))
+  colnames(perf_df) = c("threshold","true_positive_rate","false_positive_rate")
+  for(i in seq(from=0, to=1, by=increment)){
+    y_temp_pred = ifelse(y_pred>=i,1,0)
+    conf_matrix = confusionMatrix(data=factor(y_temp_pred), reference = factor(y_true))
+    fp = conf_matrix$table[2,1]
+    tp = conf_matrix$table[2,2]
+    tn = conf_matrix$table[1,1]
+    fn = conf_matrix$table[1,2]
+    #sensitivity_temp = tp/(tp+fn)
+    #specificity_temp = tn/(tn+fp)
+    fpr = fp / (fp + tn)
+    tpr = tp/(tp+fn)
+    perf_df[nrow(perf_df) + 1,] = c(i,tpr,fpr)
+  }
+  #p = (ggplot(data = perf_df,aes(x=fpr,y=tpr,group = 1)) +
+  #  geom_line())
+  p = ggplot(perf_df,aes(false_positive_rate,true_positive_rate))+geom_line(size = 2, alpha = 0.7)
+  print(p)
+  return(perf_df)
+}
+df2 = plot_roc(y_true,y_pred)
+
+###########################################
+#EXERCISE 4
+###########################################
+
+rodeo_df = read.csv("Data/rodeo.csv",header=T)
+model_rodeo=glm(achat~age,family="binomial",data=rodeo_df )
+# Question 4.1 a
+summary(model_rodeo)
+# Question 4.1 b 
+predict(model_rodeo, data.frame(age = c(35)), type="response")
+predict(model_rodeo, data.frame(age = c(50)), type="response")
+# Question 4.1 c
+age_pred_df = data.frame(matrix(ncol = 2, nrow = 0))
+colnames(age_pred_df) = c("age","prediction")
+
+for(year in seq(from=0, to=100, by=2)){
+  prediction_age = predict(model_rodeo, data.frame(age = c(year)), type="response")
+  age_pred_df[nrow(age_pred_df) + 1,] = c(year,prediction_age)
+}
+p = ggplot(age_pred_df,aes(age,prediction))+geom_line(size = 2, alpha = 0.7)
+print(p)
+
+# Question 4.1 d
+set.seed(101)
+sample <- sample.int(n = nrow(rodeo_df), size = floor(.75*nrow(rodeo_df)), replace = F)
+train_rod_df <- rodeo_df[sample, ]
+test_rod_df  <- rodeo_df[-sample, ]
+
+model_rodeo_cv=glm(achat~age,family="binomial",data=train_rod_df )
+y_pred_rod_cv = ifelse(predict(model_rodeo_cv, test_rod_df, type="response")>=0.5,1,0)
+
+length(y_pred_rod_cv)
+length(test_rod_df$achat)
+
+conf_matrix_rod_cv = confusionMatrix(data=factor(y_pred_rod_cv), reference = factor(test_rod_df$achat))
+fp_rod_cv = conf_matrix_rod_cv$table[2,1]
+tp_rod_cv = conf_matrix_rod_cv$table[2,2]
+tn_rod_cv = conf_matrix_rod_cv$table[1,1]
+fn_rod_cv = conf_matrix_rod_cv$table[1,2]
+fpr_rod_cv = fp_rod_cv / (fp_rod_cv + tn_rod_cv)
+
+################################################
+# Question 4.2
+set.seed(200)  
+n=nrow(rodeo_df)
+id.train=sample(1:n,300,replace=FALSE)
+id.valid=sample(setdiff(1:n,id.train),100,replace=FALSE)
+id.test=setdiff(setdiff(1:n,id.train),id.valid)
+
+# Division de l'échantillon
+rodeo_df.train=rodeo_df[id.train,]
+rodeo_df.valid=rodeo_df[id.valid,]
+rodeo_df.test=rodeo_df[id.test,]
+
+################################################
+# Question 4.3 a
+model_rodeo_all_var=glm(achat~.,family="binomial",data=rodeo_df.train )
+summary(model_rodeo_all_var)
+
+# Question 4.3 b
+y_pred_all_var_prob = predict(model_rodeo_all_var, rodeo_df.valid, type="response")
+y_pred_all_var = ifelse(y_pred_all_var_prob>=0.5,1,0)
+conf_matrix_all_var = confusionMatrix(data=factor(y_pred_all_var), reference = factor(rodeo_df.valid$achat))
+fp_all_var = conf_matrix_all_var$table[2,1]
+tp_all_var = conf_matrix_all_var$table[2,2]
+tn_all_var = conf_matrix_all_var$table[1,1]
+fn_all_var = conf_matrix_all_var$table[1,2]
+sensitivity_all_var = tp_all_var/(tp_all_var+fn_all_var)
+specificity_all_var = tn_all_var/(tn_all_var+fp_all_var)
+
+# Question 4.3 c
+pred_all_var = prediction(y_pred_all_var_prob,rodeo_df.valid$achat)
+perf_sn_sp = performance(pred_all_var, "tpr","fpr")
+plot(perf_sn_sp,colorize = T, lwd = 2)
+abline(a = 0, b = 1) 
+auc = performance(pred_all_var, measure = "auc")
+print(auc@y.values)
+
+################################################
+# Question 4.4
+View(rodeo_df.train)
+model_rodeo_interactions=glm(achat~(.)*age,family="binomial",data=rodeo_df.train )
+summary(model_rodeo_interactions)
+y_pred_interactions = ifelse(predict(model_rodeo_interactions, rodeo_df.valid, type="response")>=0.5,1,0)
+
+AIC(model_rodeo_interactions)
+BIC(model_rodeo_interactions)
+
+conf_matrix_interactions = confusionMatrix(data=factor(y_pred_interactions), reference = factor(rodeo_df.valid$achat))
+fp_interactions = conf_matrix_interactions$table[2,1]
+tp_interactions = conf_matrix_interactions$table[2,2]
+tn_interactions = conf_matrix_interactions$table[1,1]
+fn_interactions = conf_matrix_interactions$table[1,2]
+
+# misclassification
+mis_rate_interactions = (fp_interactions + fn_interactions)/(fp_interactions + fn_interactions + tn_interactions +tp_interactions)
+
+
+AIC(model_rodeo_all_var)
+BIC(model_rodeo_all_var)
+
+conf_matrix_all_var = confusionMatrix(data=factor(y_pred_all_var), reference = factor(rodeo_df.valid$achat))
+fp_all_var = conf_matrix_all_var$table[2,1]
+tp_all_var = conf_matrix_all_var$table[2,2]
+tn_all_var = conf_matrix_all_var$table[1,1]
+fn_all_var = conf_matrix_all_var$table[1,2]
+
+# misclassification
+mis_rate_all_var = (fp_all_var + fn_all_var)/(fp_all_var + fn_all_var + tn_all_var +tp_all_var)
+
